bec1a4dbb18c5f1d44a7f120ee00578550aadded
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-06 12:48:37 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,7 +1,10 @@
 #Github.com/Vasusen-code
 
 from pyrogram import Client
-
+import asyncio
+import uvloop
+loop = uvloop.new_event_loop()
+asyncio.set_event_loop(loop)
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
