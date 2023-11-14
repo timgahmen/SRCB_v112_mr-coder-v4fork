8fd728b2129ee6bd7c105de1ede121e671102f60
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-11-14 16:31:40 +0530

    Update utils.py

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,11 @@
 import sys
 import logging
 import importlib
+import math
+import time
 from pathlib import Path
+
+botStartTime = time.time()
 
 def load_plugins(plugin_name):
     path = Path(f"main/plugins/{plugin_name}.py")
@@ -12,3 +16,28 @@ def load_plugins(plugin_name):
     spec.loader.exec_module(load)
     sys.modules["main.plugins." + plugin_name] = load
     print("main has Imported " + plugin_name)
+
+
+def humanbytes(size):
+    """ humanize size """
+    if not size:
+        return ""
+    power = 1024
+    t_n = 0
+    power_dict = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        t_n += 1
+    return "{:.2f} {}B".format(size, power_dict[t_n])
+
+
+def TimeFormatter(seconds: float) -> str:
+    """ humanize time """
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "")
+    return tmp[:-2]
