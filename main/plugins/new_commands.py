5fe229bcbdf713effc67f770f09c8243b1a82131
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-11-14 17:07:05 +0530

    Update new_commands.py

--- main/plugins/new_commands.py
+++ main/plugins/new_commands.py
@@ -32,7 +32,7 @@ async def show_status(event):
     stt = dt.now()
     ed = dt.now()
     ms = (ed - stt).microseconds / 1000
-    p = f"🌋Pɪɴɢ = {ms}ms"
+    p = {ms}ms
     memory = virtual_memory()
     mem_t = humanbytes(memory.total)
     mem_a = humanbytes(memory.available)
