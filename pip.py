eaac4f4b23713232e4b4124a800d44f74db2f306
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-12-30 12:05:05 +0530

    Create pip.py

---
+++ pip.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def install_dependencies():
+    try:
+        subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
+        print('All dependencies have been installed successfully.')
+    except subprocess.CalledProcessError as e:
+        print('An error occurred while installing the dependencies:', e)
+
+if __name__ == "__main__":
+    install_dependencies()
