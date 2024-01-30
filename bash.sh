58ec0af4628a798e5a733931ab290ed9419b20e3
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-01-30 17:29:18 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-echo "starting Bot ~RADHE RADHE";
+echo -e "\e[1;34m$(pyfiglet -f slant "starting Bot")\n$(pyfiglet -f slant "radhe radhe")\e[0m"
 LANG=en_US.UTF-8
 export PYTHONIOENCODING=utf-8
 git clean -xdf
