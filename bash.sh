452035eb44b5e81ab1161e897161786cf7048ac3
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-01-31 14:03:35 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Add color to the starting message
-echo -e "\e[1;36m$(pyfiglet -f slant "hare")\n$(pyfiglet -f slant "krishna")\e[0m"
+echo -e "\e[1;31m$(pyfiglet -f slant "starting")\n$(pyfiglet -f slant "bot")\e[0m\n\e[1;36m$(pyfiglet -f slant "hare")\n\e[1;36m$(pyfiglet -f slant "krishna")\e[0m"
 
 # Set the language and encoding
 export LANG=en_US.UTF-8
 export PYTHONIOENCODING=utf-8
 
 # Clean git directory and run the Python main script in the background
-git clean -xdf
+git clean -xdfq
 nohup python -m main > /dev/null 2>&1 &
