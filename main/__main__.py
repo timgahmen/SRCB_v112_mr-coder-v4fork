84be9698f02eb7344d9a4ee21514896aa52952c1
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-02-13 09:27:39 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -5,7 +5,7 @@ import logging
 from . import bot
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
+                    level=logging.ALERT)
 
 path = "main/plugins/*.py"
 files = glob.glob(path)
