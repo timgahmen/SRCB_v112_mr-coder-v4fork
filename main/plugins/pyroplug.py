da1d4e3a18011ffae86bcc599ffbcb8fd3f50417
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-04-15 21:13:35 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -100,7 +100,7 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
                 height, width, duration = data["height"], data["width"], data["duration"]
                 print(f'd: {duration}, w: {width}, h:{height}')
                 try:
-                    compressed_video_path = await compress_video(file, "compressed_video")
+                    compressed_video_path = await compress_video(file, "compressed_path")
                 except Exception:
                     compressed_video_path = None
                     thumb_path = await screenshot(file, duration, sender)
@@ -108,7 +108,7 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
                     thumb_path = None
                 await client.send_video(
                     chat_id=sender,
-                    video=compressed_video_path,
+                    video=compressed_path,
                     caption=caption,
                     supports_streaming=True,
                     height=height, width=width, duration=duration, 
