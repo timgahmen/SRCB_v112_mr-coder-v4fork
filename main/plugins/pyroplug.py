e2fd26f8a22156a1776a65fbb98d8a44bb79795e
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-04-28 15:57:26 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -97,10 +97,6 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
                 print("Trying to get metadata")
                 data = video_metadata(file)
                 print("Compressing video to HEVC with hvc1 tag")
-                compressed_file = f"compressed_{file}"
-                if os.path.exists(compressed_file):
-                    os.remove(compressed_file)
-                await compress_video(file, compressed_file)
                 height, width, duration = data["height"], data["width"], data["duration"]
                 print(f'd: {duration}, w: {width}, h:{height}')
                 try:
@@ -109,14 +105,14 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
                     thumb_path = None
 
                 compressed_file = f'{sender}_compressed.mp4'
-              #  subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'quiet', '-i', file, '-c:v', 'libx265', '-tag:v', 'hvc1', '-c:a', 'copy', compressed_file])
-              #  subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'quiet', '-i', file, '-c:v', 'libx265', '-preset', 'ultrafast', '-crf', '25', '-tag:v', 'hvc1', '-c:a', 'copy', compressed_file])
-                subprocess.call(['ffmpeg', '-y', '-hide_banner', '-loglevel', 'quiet', '-i', file, '-map', '0', '-c:v', 'libx265', '-color_primaries', '1', '-color_trc', '1', '-colorspace', '1', '-pix_fmt', 'yuv420p', '-color_range', '2', '-x265-params', 'log-level=warning:level=6.1:high-tier=1:profile=main', '-preset', 'ultrafast', '-crf', '24', '-tag:v', 'hvc1', '-acodec', 'copy', '-c:s', 'copy', compressed_file])
-                name = file.split("/")[-1]
-                base_name = os.path.splitext(name)[0]
+                process = await asyncio.create_subprocess_exec(['ffmpeg', '-y', '-hide_banner', '-loglevel', 'quiet', '-i', file, '-map', '0', '-c:v', 'libx265', '-color_primaries', '1', '-color_trc', '1', '-colorspace', '1', '-pix_fmt', 'yuv420p', '-color_range', '2', '-x265-params', 'log-level=warning:level=6.1:high-tier=1:profile=main', '-preset', 'ultrafast', '-crf', '23', '-tag:v', 'hvc1', '-acodec', 'copy', '-c:s', 'copy', compressed_file])
+               # subprocess.call(['ffmpeg', '-y', '-hide_banner', '-loglevel', 'quiet', '-i', file, '-map', '0', '-c:v', 'libx265', '-color_primaries', '1', '-color_trc', '1', '-colorspace', '1', '-pix_fmt', 'yuv420p', '-color_range', '2', '-x265-params', 'log-level=warning:level=6.1:high-tier=1:profile=main', '-preset', 'ultrafast', '-crf', '24', '-tag:v', 'hvc1', '-acodec', 'copy', '-c:s', 'copy', compressed_file])
+                name = os.path.basename(file)
+                base_name, _ = os.path.splitext(name)
                 new_extension = 'mkv'
-                new_filename = base_name + '.' + new_extension
-                text = f'{new_filename}'
+                new_filename = f'{base_name}.{new_extension}'
+                text = new_filename
+                await process.wait()
                 await client.send_video(
                     chat_id=sender,
                     video=compressed_file,
